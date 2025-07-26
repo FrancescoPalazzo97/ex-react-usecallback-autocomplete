bac6ff94f7edb86e8087ddaae6beb7dc9b0cca90
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-07-26 23:21:58 +0200

    UPD - Milestone 2 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useCallback } from "react"
 const API = `http://localhost:3333/products?search=`
 
 function App() {
   const [search, setSearch] = useState('');
   const [filteredProducts, setFilteredProducts] = useState([]);
 
-  async function getProducts(query) {
-    const res = await fetch(query);
-    const data = await res.json();
-    setFilteredProducts(data);
+  const debounce = (callback, delay) => {
+    let timerId;
+    return (value) => {
+      clearTimeout(timerId);
+      timerId = setTimeout(() => {
+        callback(value);
+      }, delay);
+    }
   }
 
+  // async function getProducts(query) {
+  //   const res = await fetch(query);
+  //   const data = await res.json();
+  //   setFilteredProducts(data);
+  // }
+
+  const getProducts = useCallback(debounce(query => {
+    fetch(query)
+      .then(res => res.json())
+      .then(data => setFilteredProducts(data))
+      .catch(e => console.error(`Errore nel recupero dei dati dal server: \n\n${e}`))
+  }, 500), [])
+
   useEffect(() => {
-    getProducts(`${API}${search}`)
+    if (search.length >= 2) {
+      getProducts(`${API}${search}`)
+    } else {
+      setFilteredProducts([]);
+    }
   }, [search])
 
   return (
@@ -28,12 +49,13 @@ function App() {
               value={search}
               onChange={e => setSearch(e.target.value)}
             />
-            {search &&
+            {search.length >= 2 && filteredProducts.length > 0 &&
               <ul className="list-group results-list">
                 {filteredProducts.map(p => (
                   <li key={p.id} className="list-group-item">{p.name}</li>
                 ))}
-              </ul>}
+              </ul>
+            }
           </div>
         </div>
       </div>
