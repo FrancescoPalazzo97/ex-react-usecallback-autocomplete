47c11d427606476fb3c57389192c63cc70e58bd8
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-07-26 22:46:11 +0200

    UPD - Milestone 1 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
+import { useState, useEffect } from "react"
+const API = `http://localhost:3333/products?search=`
 
 function App() {
+  const [search, setSearch] = useState('');
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  async function getProducts(query) {
+    const res = await fetch(query);
+    const data = await res.json();
+    setFilteredProducts(data);
+  }
+
+  useEffect(() => {
+    getProducts(`${API}${search}`)
+  }, [search])
 
   return (
     <>
-
+      <div className="container">
+        <div className="row py-5">
+          <div className="col-12">
+            <h1 className="text-center text-uppercase pb-5">Cerca il tuo prodotto</h1>
+            <input
+              type="text"
+              placeholder="Cerca..."
+              className="form-control-lg w-100"
+              value={search}
+              onChange={e => setSearch(e.target.value)}
+            />
+            {search &&
+              <ul className="list-group results-list">
+                {filteredProducts.map(p => (
+                  <li key={p.id} className="list-group-item">{p.name}</li>
+                ))}
+              </ul>}
+          </div>
+        </div>
+      </div>
     </>
   )
 }
